7da135360287c2a8714b4f5758ae82cf19bc9e51
Franziska Meier <franzi.meier@gmail.com>
2020-12-28 15:40:21 -0800

    updating setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,44 @@
 # \file setup.py
 # \author Franziska Meier
 #######################################################################
-from setuptools import setup, find_packages
+"""Installation function for the differentiable-robot-model project."""
 
-install_requires = ["numpy", "torch", "pyquaternion", "hydra-core", "urdf_parser_py", "jupyter", "tqdm", "matplotlib"]
+import pathlib
+from setuptools import setup
 
+# current directory
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+DESC = ('A pytorch library that implements differentiable and learnable robot models, '
+        'which allows users to learn parameters of analytical robot models, '
+        'and/or propagate gradients through analytical robot computations such as forward kinematics.')
+
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = (HERE / "version.txt").read_text().strip()
+
+install_requires = [line.rstrip() for line in open("requirements.txt", "r")]
+
+# run setup
 setup(
-    name="differentiable_robot_model",
-    author="Facebook AI Research",
-    author_email="",
-    version=1.0,
-    packages=find_packages(),
+    name='differentiable_robot_model',
+    version=VERSION,
+    description=DESC,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Franziska Meier',
+    author_email='fmeier@fb.com',
+    python_requires=REQUIRES_PYTHON,
+    url="https://github.com/facebookresearch/differentiable-robot-model",
+    keywords='analytical robot models, differentiable, optimization',
+    packages=['differentiable_robot_model'],
     install_requires=install_requires,
     include_package_data=True,
-    zip_safe=False,
+    license="MIT",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
+
