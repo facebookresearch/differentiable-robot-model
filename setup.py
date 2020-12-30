2980f1d3014b80aa55c1d1bfa301fac1cde7fece
Franziska Meier <franzi.meier@gmail.com>
2020-12-29 17:57:50 -0800

    version update

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ DESC = ('A pytorch library that implements differentiable and learnable robot mo
         'and/or propagate gradients through analytical robot computations such as forward kinematics.')
 
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 data_files = []
 datadir = "diff_robot_data"
