fix hard-coded rotation axis (z) in inverse dynamics call to use joint axis from urdf

differentiable_robot_model/differentiable_robot_model.py
@@ -240,7 +240,11 @@ class DifferentiableRobotModel(torch.nn.Module):
         for i in range(qdd_des.shape[1]):
             idx = self._controlled_joints[i]
             rot_axis = torch.zeros((batch_size, 3)).to(self._device)
-            rot_axis[:, 2] = torch.ones(batch_size).to(self._device)
+            axis = self._bodies[idx].joint_axis[0]
+            axis_idx = int(torch.where(axis)[0])
+            rot_sign = torch.sign(axis[axis_idx])
+
+            rot_axis[:, axis_idx] = rot_sign * torch.ones(batch_size).to(self._device)
             force[:, i] += (
                 self._bodies[idx].force.ang.unsqueeze(1) @ rot_axis.unsqueeze(2)
             ).squeeze()

tests/test_allegro.py
@@ -15,9 +15,9 @@ np.set_printoptions(precision=3, suppress=True)
 torch.set_printoptions(precision=3, sci_mode=False)
 torch.set_default_tensor_type(torch.DoubleTensor)
 
-#rel_urdf_path = "allegro/urdf/allegro_hand_description_left.urdf"
+rel_urdf_path = "allegro/urdf/allegro_hand_description_left.urdf"
 #rel_urdf_path = "allegro/urdf/allegro_hand_description_left_finger.urdf"
-rel_urdf_path = "allegro/urdf/allegro_hand_description_left_finger_wotip.urdf"
+#rel_urdf_path = "allegro/urdf/allegro_hand_description_left_finger_wotip.urdf"
 urdf_path = os.path.join(robot_description_folder, rel_urdf_path)
 
 pc_id = p.connect(p.DIRECT)