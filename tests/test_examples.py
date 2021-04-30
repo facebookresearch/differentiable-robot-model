a64f3e748097ba617f8f752ccc8ea4d9c5efc28d
ExhAustin <aswang@andrew.cmu.edu>
2021-04-30 16:52:58 -0700

    Add test that runs through examples

---
+++ tests/test_examples.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import subprocess
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "relpath",
+    [
+        "../examples/learn_dynamics_iiwa.py",
+        "../examples/learn_forward_dynamics_iiwa.py",
+        "../examples/learn_kinematics_of_iiwa.py",
+        "../examples/learn_kinematics_of_toy.py",
+    ],
+)
+def test_examples(relpath):
+    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    py_file = os.path.abspath(os.path.join(curr_dir, relpath))
+    cmd = [sys.executable, py_file]
+    subprocess.check_call(cmd)
