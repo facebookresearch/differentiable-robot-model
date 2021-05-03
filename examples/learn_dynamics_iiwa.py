60f43ec49973c529d74f2b6d1a2a4af008d8a460
Austin Wang <austinswang@gmail.com>
2021-05-03 14:42:40 -0700

    Learning experiment tests (#17)
    * Add test that runs through examples
    
    * Remove two longer examples from tests
    
    * Correct dtype
    
    * Formatting
    
    * Make devices in examples toggle-able and debug
    
    * Have test run all examplse
    
    * Debug

--- examples/learn_dynamics_iiwa.py
+++ examples/learn_dynamics_iiwa.py
@@ -36,48 +36,53 @@ class NMSELoss(torch.nn.Module):
         return werr.mean()
 
 
-abs_config_dir = os.path.abspath(
-    os.path.join(differentiable_robot_model.__path__[0], "../conf")
-)
-with initialize_config_dir(config_dir=abs_config_dir):
-    learnable_robot_model_cfg = hydra_compose(
-        config_name="torch_robot_model_learnable_l4dc_constraints.yaml"
+def run(n_epochs=10, n_data=1000, device="cpu"):
+    abs_config_dir = os.path.abspath(
+        os.path.join(differentiable_robot_model.__path__[0], "../conf")
     )
-
-
-# ground truth robot model (with known kinematics and dynamics parameters) - used to generate data
-gt_robot_model = DifferentiableKUKAiiwa()
-gt_robot_model.print_link_names()
-
-train_data = generate_sine_motion_inverse_dynamics_data(
-    gt_robot_model, n_data=1000, dt=1.0 / 250.0, freq=0.05
-)
-train_loader = DataLoader(dataset=train_data, batch_size=100, shuffle=False)
-
-# learnable robot model
-urdf_path = os.path.join(
-    diff_robot_data.__path__[0], learnable_robot_model_cfg.model.rel_urdf_path
-)
-learnable_robot_model = DifferentiableRobotModel(
-    urdf_path,
-    learnable_robot_model_cfg.model.learnable_rigid_body_config,
-    learnable_robot_model_cfg.model.name,
-)
-optimizer = torch.optim.Adam(learnable_robot_model.parameters(), lr=1e-2)
-loss_fn = NMSELoss(train_data.var())
-for i in range(10):
-    losses = []
-    for batch_idx, batch_data in enumerate(train_loader):
-        q, qd, qdd_des, gt_tau = batch_data
-        optimizer.zero_grad()
-        tau_pred = learnable_robot_model.compute_inverse_dynamics(
-            q=q, qd=qd, qdd_des=qdd_des, include_gravity=True
+    with initialize_config_dir(config_dir=abs_config_dir):
+        learnable_robot_model_cfg = hydra_compose(
+            config_name="torch_robot_model_learnable_l4dc_constraints.yaml"
         )
-        loss = loss_fn(tau_pred, gt_tau)
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
 
-    print(f"i: {i} loss: {np.mean(losses)}")
+    # ground truth robot model (with known kinematics and dynamics parameters) - used to generate data
+    gt_robot_model = DifferentiableKUKAiiwa(device=device)
+    gt_robot_model.print_link_names()
 
-learnable_robot_model.print_learnable_params()
+    train_data = generate_sine_motion_inverse_dynamics_data(
+        gt_robot_model, n_data=1000, dt=1.0 / 250.0, freq=0.05
+    )
+    train_loader = DataLoader(dataset=train_data, batch_size=100, shuffle=False)
+
+    # learnable robot model
+    urdf_path = os.path.join(
+        diff_robot_data.__path__[0], learnable_robot_model_cfg.model.rel_urdf_path
+    )
+    learnable_robot_model = DifferentiableRobotModel(
+        urdf_path,
+        learnable_robot_model_cfg.model.learnable_rigid_body_config,
+        learnable_robot_model_cfg.model.name,
+        device=device,
+    )
+    optimizer = torch.optim.Adam(learnable_robot_model.parameters(), lr=1e-2)
+    loss_fn = NMSELoss(train_data.var())
+    for i in range(n_epochs):
+        losses = []
+        for batch_idx, batch_data in enumerate(train_loader):
+            q, qd, qdd_des, gt_tau = batch_data
+            optimizer.zero_grad()
+            tau_pred = learnable_robot_model.compute_inverse_dynamics(
+                q=q, qd=qd, qdd_des=qdd_des, include_gravity=True
+            )
+            loss = loss_fn(tau_pred, gt_tau)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+        print(f"i: {i} loss: {np.mean(losses)}")
+
+    learnable_robot_model.print_learnable_params()
+
+
+if __name__ == "__main__":
+    run()
--- examples/learn_kinematics_of_iiwa.py
+++ examples/learn_kinematics_of_iiwa.py
@@ -23,49 +23,55 @@ np.random.seed(1)
 torch.manual_seed(0)
 
 
-abs_config_dir = os.path.abspath(
-    os.path.join(differentiable_robot_model.__path__[0], "../conf")
-)
-# we load a learnable robot model
-with initialize_config_dir(config_dir=abs_config_dir):
-    # which parameters are learnable is specified in the config file
-    learnable_robot_model_cfg = hydra_compose(
-        config_name="torch_robot_model_learnable_kinematics.yaml"
+def run(n_epochs=3000, n_data=100, device="cpu"):
+    abs_config_dir = os.path.abspath(
+        os.path.join(differentiable_robot_model.__path__[0], "../conf")
+    )
+    # we load a learnable robot model
+    with initialize_config_dir(config_dir=abs_config_dir):
+        # which parameters are learnable is specified in the config file
+        learnable_robot_model_cfg = hydra_compose(
+            config_name="torch_robot_model_learnable_kinematics.yaml"
+        )
+
+    gt_robot_model = DifferentiableKUKAiiwa(device=device)
+    urdf_path = os.path.join(
+        diff_robot_data.__path__[0], learnable_robot_model_cfg.model.rel_urdf_path
+    )
+    learnable_robot_model = DifferentiableRobotModel(
+        urdf_path,
+        learnable_robot_model_cfg.model.learnable_rigid_body_config,
+        learnable_robot_model_cfg.model.name,
+        device=device,
     )
 
-gt_robot_model = DifferentiableKUKAiiwa()
-urdf_path = os.path.join(
-    diff_robot_data.__path__[0], learnable_robot_model_cfg.model.rel_urdf_path
-)
-learnable_robot_model = DifferentiableRobotModel(
-    urdf_path,
-    learnable_robot_model_cfg.model.learnable_rigid_body_config,
-    learnable_robot_model_cfg.model.name,
-)
-
-train_data = generate_random_forward_kinematics_data(
-    gt_robot_model, n_data=100, ee_name="iiwa_link_ee"
-)
-q = train_data["q"]
-gt_ee_pos = train_data["ee_pos"]
-
-optimizer = torch.optim.Adam(learnable_robot_model.parameters(), lr=1e-3)
-loss_fn = torch.nn.MSELoss()
-for i in range(3000):
-    optimizer.zero_grad()
-    ee_pos_pred, _ = learnable_robot_model.compute_forward_kinematics(
-        q=q, link_name="iiwa_link_ee"
+    train_data = generate_random_forward_kinematics_data(
+        gt_robot_model, n_data=n_data, ee_name="iiwa_link_ee"
     )
-    loss = loss_fn(ee_pos_pred, gt_ee_pos)
-    if i % 100 == 0:
-        print(f"i: {i}, loss: {loss}")
-        learnable_robot_model.print_learnable_params()
-    loss.backward()
-    optimizer.step()
+    q = train_data["q"]
+    gt_ee_pos = train_data["ee_pos"]
 
-print("parameters of the ground truth model (that's what we ideally learn)")
-print("gt trans param: {}".format(gt_robot_model._bodies[6].trans))
-print("gt trans param: {}".format(gt_robot_model._bodies[7].trans))
+    optimizer = torch.optim.Adam(learnable_robot_model.parameters(), lr=1e-3)
+    loss_fn = torch.nn.MSELoss()
+    for i in range(n_epochs):
+        optimizer.zero_grad()
+        ee_pos_pred, _ = learnable_robot_model.compute_forward_kinematics(
+            q=q, link_name="iiwa_link_ee"
+        )
+        loss = loss_fn(ee_pos_pred, gt_ee_pos)
+        if i % 100 == 0:
+            print(f"i: {i}, loss: {loss}")
+            learnable_robot_model.print_learnable_params()
+        loss.backward()
+        optimizer.step()
 
-print("parameters of the optimized learnable robot model")
-learnable_robot_model.print_learnable_params()
+    print("parameters of the ground truth model (that's what we ideally learn)")
+    print("gt trans param: {}".format(gt_robot_model._bodies[6].trans))
+    print("gt trans param: {}".format(gt_robot_model._bodies[7].trans))
+
+    print("parameters of the optimized learnable robot model")
+    learnable_robot_model.print_learnable_params()
+
+
+if __name__ == "__main__":
+    run()
