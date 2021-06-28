2f9ed957ad15eccf864fb99722ab7d6134efabbf
ExhAustin <aswang@andrew.cmu.edu>
2021-06-28 13:47:35 -0700

    Zero out unspecified inertial values

--- differentiable_robot_model/urdf_utils.py
+++ differentiable_robot_model/urdf_utils.py
@@ -112,9 +112,13 @@ class URDFRobotModel(object):
             body_params["com"] = com
             body_params["inertia_mat"] = inert_mat
         else:
-            body_params["mass"] = None
-            body_params["com"] = None
-            body_params["inertia_mat"] = None
-            print("no dynamics information for link: {}".format(link.name))
+            body_params["mass"] = torch.zeros((1,), device=self._device)
+            body_params["com"] = torch.zeros((1, 3), device=self._device)
+            body_params["inertia_mat"] = torch.zeros((1, 3, 3), device=self._device)
+            print(
+                "Warning: No dynamics information for link: {}, setting all inertial properties to 0.".format(
+                    link.name
+                )
+            )
 
         return body_params
